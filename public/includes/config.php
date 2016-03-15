684f7da7896ca0e9cc6b7dc7135906189d3b1bc3
Your NamecoilEraldo <youremail@example.com>
2016-03-15 00:51:17 -0400

    changes

---
+++ public/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('IN_BLOG'))
+{
+	exit;
+}
+
+$sqlconfig = array();
+$sqlconfig['username'] = 'sigmachi_bloguser';			 // database username
+$sqlconfig['password'] = 'rickdotcom';			 // database password
+$sqlconfig['host']     = 'localhost';	 // database host (usually localhost)
+$sqlconfig['dbname']   = 'sigmachi_blog';   			 // database name
+
+?>
